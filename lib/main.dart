a0177cc2d647a90c4efd4077376c39739487cc92
B <alexbardahan@Alexs-MacBook-Air.local>
2021-03-24 01:26:40 +0200

    Drawer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/filters_screen.dart';
 import './screens/tabs_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/categories_screen.dart';
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
             CategoryMealsScreen(), //instead you could use
         //'/category-meals': (ctx) => CategoryMealsScreen()
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       // onGenerateRoute: (settings) {
       //   print(settings.arguments);
